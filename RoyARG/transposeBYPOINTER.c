5cadb8b6007460ab5d0201eb3253ae03b3a9aab0
RoyARG02 <anuragr9847@gmail.com>
2018-12-01 13:48:53 +0530

    Code readability fix

--- RoyARG/transposeBYPOINTER.c
+++ RoyARG/transposeBYPOINTER.c
@@ -8,14 +8,14 @@ void main()
     printf("Enter columns: ");
     scanf("%d",&col);
     int size=row*col;
-    int *mat=(int *)malloc(size*sizeof(int));       //given matrix, dynamic 1D array
-    int *trans=(int *)malloc(size*sizeof(int));     //transpose matrix, dynamic 1D array
+    int *mat=(int *)malloc(size*sizeof(int));           //given matrix, dynamic 1D array
+    int *trans=(int *)malloc(size*sizeof(int));         //transpose matrix, dynamic 1D array
     printf("Enter elements:\n");
     for(int i=0;i<row;i++)
     {
         for(int j=0;j<col;j++)
         {
-            scanf("%d",(mat+(col*i+j)));            //'j' changes in the inner loop, 'i' changes in the outer loop in multiples of 'col'
+            scanf("%d",(mat+ (col*i+j) ) );             //'j' changes in the inner loop, 'i' changes in the outer loop in multiples of 'col'
         }
     }
     printf("The given matrix:\n");
@@ -23,16 +23,16 @@ void main()
     {
         for(int j=0;j<col;j++)
         {
-            printf("%d ",*(mat+(col*i+j)));         //same logic as above
+            printf("%d ",*(mat+ (col*i+j) ) );          //same logic as above
         }
         printf("\n");
     }
-    int k=0;                                        //index variable of 'trans' array
+    int k=0;                                            //index variable of 'trans' array
     for(int i=0;i<col;i++)
     {
         for(int j=0;j<row;j++)
         {
-            *(trans+k)=*(mat+(j*col+i));            //'row' and 'col' are interchanged to pick the elements in TRANSPOSED ORDER
+            *(trans+k)=*(mat+ (j*col+i) );              //'row' and 'col' are interchanged to pick the elements in TRANSPOSED ORDER
             k++;
         }
     }
@@ -41,7 +41,7 @@ void main()
     {
         for(int j=0;j<row;j++)
         {
-            printf("%d ",*(trans+(row*i+j)));       /*'row' and 'col' are interchanged only to print the 1D array in transposed manner
+            printf("%d ",*(trans+ (row*i+j) ) );       /*'row' and 'col' are interchanged only to print the 1D array in transposed manner
                                                        the 'trans' already has the elements in transposed way*/
         }
         printf("\n");
